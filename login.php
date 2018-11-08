9fdbf7e46e494711d3dd308b835bd09804e988b5
Indhi Farhandika <indhifarhandika@gmail.com>
2018-11-08 16:19:24 +0700

    Update 4 Login.php

--- login.php
+++ login.php
@@ -1,3 +1,8 @@
+<?php 
+  if (isset($_POST['login'])) {
+    echo '<script>alert("SIP")</script>';
+  }
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,35 +12,36 @@
     <?php include('lib/php/link.php') ?>
     <!-- End Import Link -->
     <style>
-      .bgLogin {
-          position: fixed;
-          top: 0px;
-          width: 100%;
-          height: 100%;
+      * {
+        box-sizing: border-box;
       }
-
+      .bgLogin {
+        position: fixed;
+        top: 0px;
+        width: 100%;
+        height: 100%;
+      }
       .bgLogin .full {
         background-image: url('lib/img/index2.jpeg');
         background-size: cover;
       }
-
       .bgLogin>div {
-          position: fixed;
-          top: 0px;
-          width: 100%;
-          height: 100%;
-          background-size: cover, cover;
+        position: fixed;
+        top: 0px;
+        width: 100%;
+        height: 100%;
+        background-size: cover, cover;
       }
-
       .outer {
-          display: table;
-          position: absolute;
-          min-width: 100%;
+        display: table;
+        position: absolute;
+        min-width: 100%;
+        margin-top: 12%;
       }
       .loginCenter {
+        border-radius: 20px;
         margin-left: 25%;
         margin-right: 25%;
-        border-radius: 20px;
       }
     </style>
   </head>
@@ -46,13 +52,13 @@
         </div>
       </div>
       <div class="row outer">
-        <div class="col">
-          <div class="card loginCenter mt-5">
+        <div class="col-xs">
+          <div class="card loginCenter">
             <div class="card-header">
               <h5 class="card-title text-primary fontFamily" id="exampleModalLabel" style="font-size: 40px; text-align: center;">Login</h5>
             </div>
             <div class="card-body">
-              <form name="formLogin" method="post" action="lib/php/login.php">
+              <form name="formLogin" method="post" action="">
                 <div class="form-group">
                   <label for="username">Username</label>
                   <input type="text" name="username" class="form-control" id="username" aria-describedby="usernameHelp" placeholder="Username" autocomplete="off" required>
@@ -64,7 +70,7 @@
                 <small>Belum punya akun, silahkan <a href="daftar.php">Daftar</a></small>
                 <div class="form-group text-center pt-4" style="border-top: 1px solid #e9ecef;">
                   <a class="btn btn-outline-secondary" href="index.php" role="button">Kembali</a>
-                  <button type="submit" class="btn btn-outline-primary">Login</button>
+                  <button type="submit" name="login" class="btn btn-outline-primary">Login</button>
                 </div>
               </form>
             </div>
